f68dee0ce9ca805b7a9b8f8b1b974af56a38e532
RaihanRamadhan31321 <144348683+RaihanRamadhan31321@users.noreply.github.com>
2026-01-14 17:27:14 +0700

    Sync pnpm-lock.yaml with package.json

--- app/(Landing)/page.tsx
+++ app/(Landing)/page.tsx
@@ -2,10 +2,10 @@ import HeroSection from "./Components/Home/hero";
 import CategoriesSection from "./Components/Home/categories";
 import ProductsSection from "./Components/Home/products";
 
+
 export default function Home() {
   return (
-    <main className="text-white">
-      <h1>Website Sport</h1>
+    <main>
       <HeroSection />
       <CategoriesSection />
       <ProductsSection />
